f8314a18b1b30747fdc024676847005b193a93ec
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-03-09 00:12:02 -0700

    updated with my logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,10 @@
-// create a start page with a start button 
-// counter counting down  - 120 seconds
-// prevent timer speeding up
-//     if the timer runs out end the game and display correct & incorrect
-// create an onclick event to remove the start page and display the Q&A
-//     10 travia questions 
-//     10 radial button answers - assign a value to each button (1-4)
-//     only allowed to pick one answer per question 
-// done button
-// counters for correct answers & incorrect answers
-//     if userGuess === correct answer {
-//         correctCounter++
-//     } else {
-//         incorrectCounter++
-//     }
-
 var userGuess = 0;
 var correctCounter = 0;
 var incorrectCounter = 0;
 var timerInterval;
 var timer = 20;
 
-var triviaQuestions = [{
+var questionsArray = [{
     question: "Question 1",
     answerList: ["option 1", "option 2", "option 3", "option 4"],
     answer: 1
@@ -29,25 +13,23 @@ var triviaQuestions = [{
     answerList: ["choice 1", "choice 2", "choice 3", "choice 4"],
     answer: 0
 }]
+console.log(questionsArray)
+
 // -----------------------------------------------------------------------
 
-//start button 
-$("#startBtn").on("click", function () {
-    $("#startBtn").hide();
-    $(".timer").text("Time Left: " + timer);
-    displayQuestions();
-    runTimer();
 
-})
-
-//start timer
+//prevent the timer from speeding up
+//START TIMER
 function runTimer() {
     clearInterval(timerInterval);
     timerInterval = setInterval(decrement, 1000);
 
 }
 
-//countdown timer
+//count down the timer
+//if the counter reached 0, go the to results page
+//alter "time's up"
+//COUNTDOWN TIMER
 function decrement() {
     timer--;
     $(".timer").text("Time Left: " + timer);
@@ -61,59 +43,63 @@ function decrement() {
     }
 }
 
+
+// display 10 travia questions & answers
+    // only allowed to pick one answer per question 
+    // assign a value to each button (1-4)
+// counters for correct answers & incorrect answers
+//     if userGuess === correct answer {
+//         correctCounter++
+//     } else {
+//         incorrectCounter++
+//     }
 function displayQuestions() {
-    var newForm = $("<form>");
-    var submitBtn = $("<button>").addClass("btn btn-primary").attr("id", "submitBtn").attr("type", "submit").text("Submit");
-    var newDiv = $("<div>");
-    var newPar = $("<p>");
-    
-    //RADIO BUTTON
-    var RadioBtn = $("<button>"); 
-        RadioBtn.addClass("form-check-input");
-        RadioBtn.attr("type", "radio");
-        RadioBtn.attr("name", "inlineRadio1");
-        RadioBtn.html("<button>")
-    
-    
-    // <input class= type="radio" name="inlineRadio1" value="2">
-    
-    //RADIO LABEL - TEXT NEXT TO 
-    var RadioLabel = $("<label>"); 
-        RadioLabel.addClass("form-check-label");
-        RadioLabel.attr("for", "inlineRadio1");
 
-    {/* <label class="form-check-label" for="inlineRadio1">3</label> */ }
+    for (var i = 0; i < questionsArray.length; i++) {
+    var question = $("<p>");
+    question.addClass("triviaQuestion");
+    question.text(questionsArray[i].question)
+    $("#questions").append(question);
 
-    for (var i = 0; i < triviaQuestions.length; i++) {
-        newPar.text(triviaQuestions[i].question);
-        newForm.append(newPar);
-        newPar = $("<p>");
-
-        // RadioBtn.html(triviaQuestions[i].answerList);
-        // newForm.append(RadioBtn);
-        // RadioBtn = $("<input>");
-
-        RadioLabel.text(triviaQuestions[i].answerList);
-        newForm.append(RadioLabel);
-        RadioLabel = $("<label>");
-
-
-        //follow this pattern for the other attrib. 
-    }
-
-    $(".test").append(newForm.append(submitBtn));
+    var answerOptions = $("<button>");
+    answerOptions.addClass("answerOptions");
+    answerOptions.attr("data-answerOption", questionsArray[i].answerList[i]-1);
+    answerOptions.text(questionsArray[i].answerList[i]);
+    $("#questions").append(answerOptions);
+}
 
 
 }
 
+// on click event for done button
+    // hide questions
+    // show results
+// DONE BUTTON
+
+
+//hide the questions 
+//hide the timer
+//add the correct answers & incorrect answer
+//display correct answers & incorrect answers on the page
 function endPage() {
 
 
 }
 
-
-
-
 // -----------------------------------------------------------------------
+// start page with a start button 
+// create an onclick event
+    //hide start button
+    //display the timer
+    //run the timer funtion
+    //display questions
+    
+//START BUTTON
+$("#startBtn").on("click", function () {
+    $("#startBtn").hide();
+    $(".timer").text("Time Left: " + timer);
+    runTimer();
+    displayQuestions();
 
+})
 
